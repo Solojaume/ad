5f3824c874b85b0059c78e0d1b7d753c27fda8e5
SoloJaume <jaumeponsorti@gmail.com>
2019-10-02 11:27:21 +0200

    dbpruebas.sql añadido y carpeta mysqldatab añadida

---
+++ dbpruebas.sql
@@ -0,0 +1,27 @@
+Create table categoria(
+    id serial primary key,
+    nombre varchar(50) unique not null
+);
+
+/*Ejercicio crear una tabla llamada articulos con los campos:
+    -id
+    -nombre
+    -precio
+    -categoria: bigint
+Menu :
+    -Editar
+    -Crear
+    -Consultar
+    -Eliminar 
+    -Listar*/
+CREATE TABLE `dbprueba`.`articulos` ( 
+    `id` SERIAL NOT NULL AUTO_INCREMENT , 
+    `nombre` VARCHAR(50) NOT NULL , 
+    `precio` DECIMAL(10,2) NULL , 
+    `categoria` BIGINT UNSIGNED NOT NULL ,
+     PRIMARY KEY (`id`),
+     FOREIGN KEY (`categoria`) REFERENCES cliente(`id`)
+ ) ENGINE = InnoDB;
+
+INSERT INTO `articulo` (`id`, `nombre`, `precio`, `categoria`) VALUES (NULL, 'ARTICULO1', '9.99', '1')
+INSERT INTO `articulo` ( `nombre`, `precio`, `categoria`) VALUES ('ARTICULO1', null, '1')
