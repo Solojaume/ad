3f00182255cccb8be722a02ba17a576959100017
SoloJaume <jaumeponsorti@gmail.com>
2019-10-07 10:32:02 +0200

    Mejorado la clase menu de CArticulo

--- CArticulo/CArticulo/Menu.cs
+++ CArticulo/CArticulo/Menu.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CArticulo
 {
     public class Menu
     {
-        public static Menu Create(string menuLabel)
-        {
+        private bool exit = false;
+        private IList<string> labels = new List<string>();
+        private IDictionary<string, Action> actions =
+            new Dictionary<string, Action>();
+
+        public static Menu Create(string menuLabel) {
             return new Menu();
         }
-        public Menu Add(string label,Action action)
+
+        public Menu Add(string label, Action action) {
+            labels.Add(label);
+            string option = label.Substring(0, 1).ToUpper();
+            actions.Add(option, action);
+            return this;
+        }
+
+        public Menu ExitWhen(string label) {
+            labels.Add(label);
+            string option = label.Substring(0, 1).ToUpper();
+            actions.Add(option, () => exit = true);
+            return this;
+        }
+
+        public void Loop() {
+            while (!exit) {
+                foreach (string label in labels)
+                    Console.WriteLine(label);
+                string option = Console.ReadLine();
+                if (actions.ContainsKey(option))
+                    actions[option]();
+            }
+
+        }
     }
 }
--- CArticulo/CArticulo/Program.cs
+++ CArticulo/CArticulo/Program.cs
@@ -6,7 +6,40 @@ namespace CArticulo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu.Create("Menu Categorías")
+                .Add("1-Nuevo", nuevo)
+                .Add("2-Editar", editar)
+                .Add("3-Borrar", borrar)
+                .Add("4-Listar", listar)
+                .ExitWhen("0 - Salir")
+                .Loop();
+            
         }
+
+        private static void nuevo() {
+            Console.WriteLine("Ha entrado en nuevo");
+            Console.ReadLine();
+        }
+
+        private static void editar() {
+            Console.WriteLine("Ha entrado en editar");
+            Console.ReadLine();
+        }
+
+      
+
+        private static void borrar() {
+            Console.WriteLine("Ha entrado en borrar");
+            Console.ReadLine();
+        }
+
+       
+
+
+        private static void listar() {
+
+        }
+
+
     }
 }
