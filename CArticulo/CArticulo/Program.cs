2209ab6685f966d34410e8fb1a1fcf2f210a41da
SoloJaume <jaumeponsorti@gmail.com>
2019-10-11 09:48:06 +0200

    Añadido a CArticulo metodos listar y nuevo

--- CArticulo/CArticulo/Program.cs
+++ CArticulo/CArticulo/Program.cs
@@ -1,24 +1,43 @@
 ﻿using System;
-
+using Serpis.Ad;
+using System.Data;
+using MySql.Data.MySqlClient;
 namespace CArticulo
 {
     class MainClass
     {
+        private static IDbConnection dbConnection = new MySqlConnection(
+              "server=localhost;database=dbprueba;uid=root;pwd=sistemas;ssl-mode=none"
+           );
+        private static int obtion = 0;
+
         public static void Main(string[] args)
         {
-            Menu.Create("Menu Categorías")
+            dbConnection.Open();
+            Menu.Create("Menu Aticulo")
                 .Add("1-Nuevo", nuevo)
                 .Add("2-Editar", editar)
                 .Add("3-Borrar", borrar)
                 .Add("4-Listar", listar)
                 .ExitWhen("0 - Salir")
                 .Loop();
-            
+            dbConnection.Close();
         }
 
         private static void nuevo() {
-            Console.WriteLine("Ha entrado en nuevo");
-            Console.ReadLine();
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            Console.WriteLine("Introduce el nombre del articulo: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Introduce el precio del articulo: ");
+            string precio = Console.ReadLine();
+            Console.WriteLine("Introduce el categoria del articulo: ");
+            string categoria = Console.ReadLine();
+
+            dbCommand.CommandText = string.Format("insert into Articulo(nombre, precio, categoria) values(@nombre,@precio,@categoria)");
+            DBCommmandHelper.AddParameter(dbCommand, "nombre", nombre);
+            DBCommmandHelper.AddParameter(dbCommand, "precio", precio);
+            DBCommmandHelper.AddParameter(dbCommand, "categoria", categoria);
+            dbCommand.ExecuteNonQuery();
         }
 
         private static void editar() {
@@ -29,7 +48,8 @@ namespace CArticulo
       
 
         private static void borrar() {
-            Console.WriteLine("Ha entrado en borrar");
+            Console.WriteLine("Introduce el id del articulo que quieras borrar:" +
+            	"");
             Console.ReadLine();
         }
 
@@ -37,8 +57,18 @@ namespace CArticulo
 
 
         private static void listar() {
+            if(obtion=)
+            Console.WriteLine("Muestro todos los articulos de la lista");
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = "select * from articulo ";
+            IDataReader dataReader = dbCommand.ExecuteReader();
 
+
+            while (dataReader.Read()) {
+                Console.WriteLine("id={0} nombre={1} precio={2} categoria={3}", dataReader["id"], dataReader["nombre"], dataReader["precio"], dataReader["categoria"]);
+            }
         }
+
 
 
     }
