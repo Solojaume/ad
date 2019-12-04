2f102147a325e51b8f7276d95f3caf0c86fd0dc7
SoloJaume <jaumeponsorti@gmail.com>
2019-12-04 12:15:09 +0100

    añadido

---
+++ dbprueba2.sql
@@ -0,0 +1,20 @@
+START TRANSACTION;
+CREATE DATABASE dbprueva2;
+CREATE TABLE `clientes` (
+  `id_cliente` bigint(20) UNSIGNED NOT NULL,
+  `dni` varchar(100) DEFAULT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `teleforno` decimal(10,2) DEFAULT NULL,
+   PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `paquetes` (
+  `id_paquete` bigint(20) UNSIGNED NOT NULL,
+  `cantidad` bigint(250) DEFAULT NULL,
+  `precio` bigint(200) DEFAULT NULL,
+  `id_cliente` bigint(20) UNSIGNED DEFAULT NULL,
+   PRIMARY KEY (`id_paquete`),
+   FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+   KEY `id_cliente` (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+Comit;
